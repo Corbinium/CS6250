015d5151aabb5938a47f19c021024366342e0253
Corbin Barney <corbbarney@gmail.com>
2026-02-12 16:03:18 -0700

    made the operators

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CS6250
+
+Run all the code below and you'll be good to go.
+
+```bash
+python3 -m venv venv             # create virtual environment
+source venv/bin/activate         # run venv
+pip3 install -r requirements.txt # install packages
+python3 -i operators.py          # run system
+```
---
+++ operators.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+
+_NUM_QUBITS = 3
+
+class operator:
+    """operator object
+    Attributes:
+        scaler: scalar multiplier for the operator
+        matrix: matrix representation of the operator
+    Methods:
+        from_int(value: int) -> tuple[operator, operator]: creates operators from an integer, 
+            the first operator represents the state of the qubits and the second operator 
+            represents the state specific normalization operator
+        operate(other: operator) -> operator: applies the operator to another operator and 
+            returns the result
+        state() -> str: returns the state represented by the operator as a binary string, this 
+            will only work for single column vectors
+    """
+    scaler: np.float64
+    matrix: np.ndarray
+
+    def __init__(self, scaler: np.float64, matrix: np.ndarray):
+        self.scaler = scaler
+        self.matrix = matrix
+
+    @classmethod
+    def from_int(cls, value: int) -> tuple['operator', 'operator']:
+        state = f"{value:0{_NUM_QUBITS}b}"
+        q_list = [Q_0 if bit == '0' else Q_1 for bit in state]
+        op_q = tensor_series([operator(1, q) for q in q_list])
+        a_list = [OP_X if bit == '0' else OP_I for bit in state]
+        op_a = tensor_series(a_list)
+        return op_q, op_a
+
+    def __str__(self):
+        return f"Scale: {self.scaler}\n{np.array2string(self.matrix.real, precision=2, suppress_small=False)}"
+
+    def __repr__(self):
+        return self.__str__()
+    
+    def operate(self, other: 'operator') -> 'operator':
+        return operator(self.scaler * other.scaler, self.matrix @ other.matrix)
+    
+    def state(self) -> str:
+        index = np.argmax(np.abs(self.matrix))
+        state = f"{index:0{_NUM_QUBITS}b}"
+        return state
+
+def tensor_series(ops: list[operator]) -> operator:
+    """tensor_series(ops: list[operator]) -> operator
+        ops: list of operators to be tensored together in the order to be applied
+        returns: operator resulting from the tensor product of the input operators
+    """
+    result_matrix = ops[0].matrix
+    for op in ops[1:]:
+        result_matrix = np.kron(result_matrix, op.matrix)
+    return operator(np.prod([op.scaler for op in ops]), result_matrix)
+
+def operator_series(ops: list[operator]) -> operator:
+    """operator_series(ops: list[operator]) -> operator:
+        ops: list of operators to be applied in the order to apply them
+        returns: operator resulting from the application of the input operators in sequence
+    """
+    result = ops[0]
+    for op in ops[1:]:
+        result = result.operate(op)
+    return result
+
+Q_0 = np.array([[1], [0]])
+Q_1 = np.array([[0], [1]])
+
+OP_X = operator(1, np.array([[0, 1], [1, 0]]))
+OP_Z = operator(1, np.array([[1, 0], [0, -1]]))
+OP_H = operator(1/np.sqrt(2), np.array([[1, 1], [1, -1]]))
+OP_I = operator(1, np.array([[1, 0], [0, 1]]))
+
+op_x = tensor_series([OP_X] * _NUM_QUBITS)
+op_h = tensor_series([OP_H] * _NUM_QUBITS)
+op_i = tensor_series([OP_I] * _NUM_QUBITS)
+op_mcz = tensor_series([OP_I] * _NUM_QUBITS)
+op_mcz.matrix[-1, -1] = -1
+
+def show_help():
+    print(f"{'='*30}\n=== Single Qubit Operators")
+    print("    OP_X, OP_Z, OP_H, OP_I\n")
+
+    print(f"{'='*30}\n=== {_NUM_QUBITS}-Qubit Operators")
+    print("    op_x, op_h, op_i, op_mcz\n")
+
+    print(f"{'='*30}\n=== Functions")
+    print(tensor_series.__doc__)
+    print(operator_series.__doc__)
+
+    print(f"{'='*30}\n=== operator")
+    print(operator.__doc__)
+
+show_help()
